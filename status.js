639b465a481025306a2f013271bd1c8aa2c0ae82
FreeSoR <140195667+FreeSoR@users.noreply.github.com>
2026-06-06 19:06:14 +0200

    status.js

--- status.js
+++ status.js
@@ -4,19 +4,18 @@ const { status } = require("minecraft-server-util");
 
 const app = express();
 
-// ---- НАСТРОЙКИ ----
+// ---- SETTINGS ----
 const WEBHOOK_URL = "https://discord.com/api/webhooks/1512843513342202078/-VDQuKn1ORl6NEgUsSMGH6gSgyxR7JrLRgqWTQYc1Qut-dGXp0y6Bbhuqs0y2TaMZF38";
 const SERVER_IP = "chloches.play.hosting";
-const PORT = null;
 
-// ---- чтобы Render не засыпал ----
+// ---- keep Render alive ----
 app.get("/", (req, res) => {
   res.send("Minecraft status bot is running");
 });
 
 app.listen(process.env.PORT || 3000);
 
-// ---- логика Discord ----
+// ---- logic ----
 let messageId = null;
 let lastState = null;
 
@@ -24,24 +23,21 @@ console.log("Bot started");
 
 async function updateStatus() {
   let data = null;
-  let ping = "N/A";
 
   try {
-    const start = Date.now();
     data = await status(SERVER_IP);
-    ping = Date.now() - start + "ms";
   } catch (e) {
     data = null;
-    ping = "N/A";
   }
 
   const online = data ? "🟢 Online" : "🔴 Offline";
-  const players = data ? `${data.players.online}/${data.players.max}` : "0/0";
+  const players = data
+    ? `${data.players.online}/${data.players.max}`
+    : "0/0";
 
   const currentState = JSON.stringify({
     online: !!data,
-    players: data ? data.players.online : 0,
-    ping
+    players: data ? data.players.online : 0
   });
 
   if (currentState === lastState) return;
@@ -60,14 +56,9 @@ async function updateStatus() {
             inline: true
           },
           {
-            name: "Ping",
-            value: ping,
+            name: "Players",
+            value: players,
             inline: true
-          },
-          {
-          name: "Latency",
-          value: `~${ping} (Render → server)`,
-          inline: true
           },
           {
             name: "Server IP",
@@ -79,7 +70,7 @@ async function updateStatus() {
             value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
             inline: false
           }
-        ],
+        ]
       }
     ]
   };
@@ -97,6 +88,6 @@ async function updateStatus() {
   }
 }
 
-// обновление каждые 15 секунд
+// run every 15 seconds
 setInterval(updateStatus, 15000);
 updateStatus();
